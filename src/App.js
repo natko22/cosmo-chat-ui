f3b47ee429f4f83a37e098bc4824677c8f50aa38
Natassa <112552395+natko22@users.noreply.github.com>
2024-08-05 09:15:38 +0200

    delete login pge

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Login from "./pages/auth/Login";
 import Chat from "./pages/Chat";
 import Activity from "./pages/Activity";
 import ActivityDashboard from "./pages/ActivityDashboard";
@@ -12,7 +11,6 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/chat" element={<Chat />} />
         <Route path="/activity" element={<Activity />} />
         <Route path="/dashboard" element={<ActivityDashboard />} />
